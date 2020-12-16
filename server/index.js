4e11378a30aa73655e18cf1e95c67bf3eda52c57
Zobair Qauomi <Zobairq55@gmail.com>
2020-12-16 19:28:28 +0100

    Add routing for production to make sure that react is served properly when deployed.

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 require("./models/User");
 require("./services/passport");
 mongoose.connect(keys.mongoConnectionString, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const app = express();
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys:[keys.cookieKey]
+    keys: [keys.cookieKey],
   })
 );
 
@@ -25,5 +25,14 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "product") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
